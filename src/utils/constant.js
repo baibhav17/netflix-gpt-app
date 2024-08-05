9f5f83ace148b5c95e4edd1394e2a780afcdf2d8
baibhav17 <baibhavsxn.035@gmail.com>
2024-08-05 21:39:21 +0530

    removed api key

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,11 +1,11 @@
-export const TMDB_API_KEY = '53184e92e70a99fcc7aa19c3c163e5a0';
+export const TMDB_API_KEY = 'YOUR_API_KEY'
 
-export const TMDB_API_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzE4NGU5MmU3MGE5OWZjYzdhYTE5YzNjMTYzZTVhMCIsIm5iZiI6MTcxOTc2ODg4OS42ODI0NjUsInN1YiI6IjY2ODE3NDYyODBmOTcyZDFmNGRiZDhhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F0fO6iUs-o00HR1-nvVovLf5BNhzMTIubz8TM3prhek'
+export const TMDB_API_ACCESS_TOKEN = 'YOUR_API_TOKEN'
 
 export const API_OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzE4NGU5MmU3MGE5OWZjYzdhYTE5YzNjMTYzZTVhMCIsIm5iZiI6MTcxOTc2OTAxOC4wOTgxMjksInN1YiI6IjY2ODE3NDYyODBmOTcyZDFmNGRiZDhhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FlvfszqrooJq6QezFvbC2K4lNJTmbTxu2qv0osnxSF8'
+      Authorization: 'Bearer YOUR__KEY'
     }
   };
